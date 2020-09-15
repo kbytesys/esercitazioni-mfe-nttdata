8974ef2e910dc48403e5dcb8f2accc51d0a1fa7c
brainrepo <murru7@gmail.com>
2020-09-15 16:27:44 +0200

    Comunicazione child-chchild

--- 06-comminication-patterns/parent-child/team-checkout/static/fragment.js
+++ 06-comminication-patterns/parent-child/team-checkout/static/fragment.js
@@ -1,55 +1,54 @@
-(function(){
+(function () {
+  const prodotti = [
+    {
+      id: 1,
+      prices: {
+        standard: "10",
+        platinum: "50",
+      },
+    },
+    {
+      id: 2,
+      prices: {
+        standard: "20",
+        platinum: "30",
+      },
+    },
+  ];
 
-    const prodotti = [
-        {
-          id: 1,
-          prices: {
-            standard: "10",
-            platinum: "50",
-          },
-        },
-        {
-          id: 2,
-          prices: {
-            standard: "20",
-            platinum: "30",
-          },
-        }
-      ];
-      
-      class BuyButton extends HTMLElement {
-        constructor() {
-          super();
-      
-          var sh = this.attachShadow({ mode: "open" });
-          this.wrapper = document.createElement("div");
-          sh.appendChild(this.wrapper);
-      
-          this.idprodotto = this.getAttribute('idprodotto');
-          this.version = this.getAttribute('version');
-        }
-      
-        static get observedAttributes() {
-          return ["idprodotto", "version"];
-        }
-      
-        attributeChangedCallback(name, oldV, newV) {
-          if (name === "idprodotto") {
-              this.render(newV, this.version);
-          }
-          if(name === "version"){
-              this.render(this.idprodotto, newV);
-          }
-        }
-      
-        connectedCallback() {
-          this.render(this.idprodotto, this.version);
-        }
-      
-        disconnectedCallback() {}
-      
-        render(idProdotto, version) {
-          this.wrapper.innerHTML = `
+  class BuyButton extends HTMLElement {
+    constructor() {
+      super();
+
+      var sh = this.attachShadow({ mode: "open" });
+      this.wrapper = document.createElement("div");
+      sh.appendChild(this.wrapper);
+
+      this.idprodotto = this.getAttribute("idprodotto");
+      this.version = this.getAttribute("version");
+    }
+
+    static get observedAttributes() {
+      return ["idprodotto", "version"];
+    }
+
+    attributeChangedCallback(name, oldV, newV) {
+      if (name === "idprodotto") {
+        this.render(newV, this.version);
+      }
+      if (name === "version") {
+        this.render(this.idprodotto, newV);
+      }
+    }
+
+    connectedCallback() {
+      this.render(this.idprodotto, this.version);
+    }
+
+    disconnectedCallback() {}
+
+    render(idProdotto, version) {
+      this.wrapper.innerHTML = `
                   <style>
                       .platinum{
                           width: 100%;
@@ -72,10 +71,16 @@
                   </style>
                   <button class="${version}">BUY NOW ${prodotti[idProdotto].prices[version]} EURO !</button>
               `;
-        }
-      }
-      
-      customElements.define("buy-button", BuyButton);
-    
-})();
+      this.wrapper.querySelector("button").addEventListener("click", () => {
+        this.dispatchEvent(
+          new CustomEvent("checkout:item_added", {
+            bubbles: true,
+            detail: { price: prodotti[idProdotto].prices[version] },
+          })
+        );
+      });
+    }
+  }
 
+  customElements.define("buy-button", BuyButton);
+})();
